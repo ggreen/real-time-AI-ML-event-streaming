83811bc8dd928524350f87265a59acbeced18e89
ggreen <gregoryg@vmware.com>
2024-08-15 14:11:13 -0400

    refactoring apps

---
+++ applications/web-ai-app/src/main/java/realtime/ai/ml/event/streaming/web/HttpModelConfig.java
@@ -0,0 +1,27 @@
+package realtime.ai.ml.event.streaming.web;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
+import realtime.ai.ml.event.streaming.web.nlp.vectors.Text2Vectors;
+import realtime.ai.ml.event.streaming.web.nlp.vectors.Text2VectorsHttpService;
+
+import java.net.URI;
+
+@Profile("httpModel")
+@Configuration
+public class HttpModelConfig {
+
+    @Value("${ai.model.uri}")
+    private String uri;
+
+    @SneakyThrows
+    @Bean
+    public Text2Vectors fakeText2Vectors()
+    {
+        return new Text2VectorsHttpService(new RestTemplate(),new URI(uri));
+    }
+}
