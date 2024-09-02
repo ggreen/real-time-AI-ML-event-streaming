398ada826f073e6b67931d80434bc030c5d2a13e
ggreen <gregoryg@vmware.com>
2024-09-02 10:33:37 -0400

    adding main html search formatting

--- applications/web-ai-app/src/main/resources/static/scripts/site.js
+++ applications/web-ai-app/src/main/resources/static/scripts/site.js
@@ -5,10 +5,11 @@ function decorateSentiment(letterResult)
 {
     var sentimentHtml = "";
 
+    //w3-block
     if( "POSITIVE" == letterResult.sentiment )
-        sentimentHtml = "<button class='w3-button w3-block w3-green w3-section' title='Accept'>positive <i class='fa fa-check'></i></button>";
+        sentimentHtml = "<button class='w3-button w3-green w3-section' title='Accept'>positive <i class='fa fa-check'></i></button>";
     else
-        sentimentHtml = "<button class='w3-button w3-block w3-red w3-section' title='Decline'>negative <i class='fa fa-remove'></i></button>";
+        sentimentHtml = "<button class='w3-button w3-red w3-section' title='Decline'>negative <i class='fa fa-remove'></i></button>";
 
 
     return     sentimentHtml;
